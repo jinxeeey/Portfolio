1135a5de1a7f22811d4046fbbc3b6014f364a26d
jinxeeey <141046191+jinxeeey@users.noreply.github.com>
2023-08-03 19:44:06 +0100

    Contact us

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,22 +1,28 @@
-function sendMail() {
-    var templateParams  = {
-      name: document.getElementById("name").value,
-      email: document.getElementById("email").value,
-      message: document.getElementById("message").value,
-    };
-  
-    const serviceID = "service_t00olvi";
-    const templateID = "template_yhlxc4p";
-  
-      emailjs.send(serviceID, templateID,templateParams);
-      .then(res=>{
-          document.getElementById("name").value = "";
-          document.getElementById("email").value = "";
-          document.getElementById("message").value = "";
-          console.log(res);
-          alert("Your message sent successfully!!")
-  
-      })
-      .catch(err=>console.log(err));
-  
-  }
+// JavaScript code in script.js file
+function sendMail(event) {
+  event.preventDefault(); // Prevent the default form submission
+
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    phone: document.getElementById("phone").value, // Fix the parameter name here
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_t00olvi";
+  const templateID = "template_yhlxc4p";
+
+  emailjs.send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("phone").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("Your message has been sent!");
+    })
+    .catch((err) => console.log(err));
+}
+
+// Add event listener to the form submit button
+document.getElementById('send').addEventListener('click', sendMail);
